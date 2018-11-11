0bffa5c7a25e74402b168a37ac492586920121ca
tom-englert <mail@tom-englert.de>
2018-11-11 18:28:23 +0100

    Remove unused code

--- Lazy.Fody/SystemReferences.cs
+++ Lazy.Fody/SystemReferences.cs
@@ -3,27 +3,19 @@
 namespace Lazy.Fody
 {
     using System;
-    using System.Linq;
     using System.Threading;
 
     using FodyTools;
 
-    using global::Fody;
-
     using JetBrains.Annotations;
 
     using Mono.Cecil;
-    using Mono.Cecil.Rocks;
 
     internal class SystemReferences
     {
-        public global::Fody.TypeSystem TypeSystem { get; }
-
         public TypeReference LazyTypeReference { get; }
 
         public TypeReference FuncTypeReference { get; }
-
-        public TypeReference LazyThreadSafetyModeReference { get; }
 
         public MethodReference LazyConstructorReference { get; }
 
@@ -33,10 +25,7 @@ namespace Lazy.Fody
 
         public SystemReferences([NotNull] ITypeSystem typeSystem)
         {
-            TypeSystem = typeSystem.TypeSystem;
-            
             LazyTypeReference = typeSystem.ImportType<Lazy<T>>();
-            LazyThreadSafetyModeReference = typeSystem.ImportType<LazyThreadSafetyMode>();
             LazyConstructorReference = typeSystem.ImportMethod(() => new Lazy<T>(() => default(T), default(LazyThreadSafetyMode)));
             LazyValueGetterReference = typeSystem.ImportPropertyGet(() => default(Lazy<T>).Value);
             
