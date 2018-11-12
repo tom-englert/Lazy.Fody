48c1ce2d3f2e3df57546e9f7fdac9d9fd80bbc39
tom-englert <mail@tom-englert.de>
2018-11-12 12:11:30 +0100

    sign runtime assembly and add comments to attribute

--- Lazy/Attributes.cs
+++ Lazy/Attributes.cs
@@ -3,11 +3,24 @@
     using System;
     using System.Threading;
 
+    /// <summary>
+    /// Apply this attribute to a read only property to implement the lazy pattern using <see cref="Lazy{T}"/>.<para/>
+    /// See <see href="https://github.com/tom-englert/Lazy.Fody"/> for details.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class LazyAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the threading mode passed to the constructor of <see cref="Lazy{T}"/>.
+        /// </summary>
         public LazyThreadSafetyMode Mode { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LazyAttribute"/> class.<para/>
+        /// Apply this attribute to a read only property to implement the lazy pattern using <see cref="Lazy{T}"/>.<para/>
+        /// See https://github.com/tom-englert/Lazy.Fody for details.<para/>
+        /// </summary>
+        /// <param name="mode">The threading mode passed to the constructor of <see cref="Lazy{T}"/>.</param>
         public LazyAttribute(LazyThreadSafetyMode mode = default(LazyThreadSafetyMode))
         {
             Mode = mode;
